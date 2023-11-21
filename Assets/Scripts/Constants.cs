11457b9d1eb0aad2ff819505c4c4b1c0a0b892d6
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-21 17:22:11 +0700

    add scenes path in constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Constants : MonoBehaviour
+public class Constants
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public const string EntryScene = "Scenes/Entry";
+	public const string GameScene = "Scenes/Game";
+	public const string HomeScene = "Scenes/Home";
 }
