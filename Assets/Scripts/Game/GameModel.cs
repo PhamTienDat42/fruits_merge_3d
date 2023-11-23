55966fab32a3b9d8a32fff87aa1470ebbab07ccd
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-23 16:11:47 +0700

    setup in gameplay

--- Assets/Scripts/Game/GameModel.cs
+++ Assets/Scripts/Game/GameModel.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameModel : MonoBehaviour
+namespace Game
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public class GameModel : MonoBehaviour
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
 }
--- Assets/Scripts/Game/GameView.cs
+++ Assets/Scripts/Game/GameView.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameView : MonoBehaviour
+namespace Game
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public class GameView : MonoBehaviour
+	{
+		public void OnEscButtonClick()
+		{
+			SceneManager.LoadScene(Constants.HomeScene);
+		}
+	}
 }
