35c7e71662ed20356224ed6da220c32fdb401235
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-23 10:49:02 +0700

    add Tag Services and targetFrameRate const

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,4 +3,10 @@ public class Constants
 	public const string EntryScene = "Scenes/Entry";
 	public const string GameScene = "Scenes/Game";
 	public const string HomeScene = "Scenes/Home";
+
+	//Tags
+	public const string ServicesTag = "Services";
+
+	//Nums
+	public const int TargetFrameRate = 60;
 }
