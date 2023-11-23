08b9ee7ecf4c4d46751bd9ce8702007c5ebfd7ca
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-23 11:05:12 +0700

    add Load gamePlay from home view

--- Assets/Scripts/Home/HomeView.cs
+++ Assets/Scripts/Home/HomeView.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Home
 {
 	public class HomeView : MonoBehaviour
 	{
-
+		public void OnPlayButtonClick()
+		{
+			SceneManager.LoadScene(Constants.GameScene);
+		}
 	}
 }
