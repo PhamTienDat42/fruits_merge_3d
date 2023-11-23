5bf69212079d4fc6af9e6c224d59263421614787
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-23 10:49:54 +0700

    get GameServices and setup in EntryController

--- Assets/Scripts/Entry/EntryController.cs
+++ Assets/Scripts/Entry/EntryController.cs
@@ -1,9 +1,36 @@
+using Services;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Entry
 {
 	public class EntryController : MonoBehaviour
 	{
+		[SerializeField] private EntryModel entryModel;
 
+		private GameServices gameServices = null;
+
+		private void Awake()
+		{
+			if (GameObject.FindGameObjectWithTag(Constants.ServicesTag) == null)
+			{
+				GameObject gameServicesObject = new(nameof(GameServices))
+				{
+					tag = Constants.ServicesTag
+				};
+				gameServices = gameServicesObject.AddComponent<GameServices>();
+			}
+
+			gameServices.AddService(new PlayerServices());
+
+			//---------------------GetServices----------------------------------------
+			var playerServices = gameServices.GetService<PlayerServices>();
+		}
+
+		private void Start()
+		{
+			Application.targetFrameRate = Constants.TargetFrameRate;
+			SceneManager.LoadScene(Constants.HomeScene);
+		}
 	}
 }
