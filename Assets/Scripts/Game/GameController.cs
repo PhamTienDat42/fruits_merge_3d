e9ca2ebded416c36f29cdced1dfaf16f42956865
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-28 18:42:16 +0700

    add tool auto drop and fix startY off fruits

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -7,14 +7,21 @@ namespace Game
 {
 	public class GameController : MonoBehaviour
 	{
-		[SerializeField] private List<Material> fruitMaterials;
 		[SerializeField] private List<Mesh> fruitMesh;
 		[SerializeField] private Camera mainCamera;
 		[SerializeField] private FruitManager fruitManager;
 
+		[Space(8.0f)]
+		[Header("Bound-Collider2D")]
+		[SerializeField] private BoxCollider2D topCollider;
+		[SerializeField] private BoxCollider2D leftCollider;
+		[SerializeField] private BoxCollider2D rightCollider;
+
 		private Vector3 startPos;
 		private bool isClickable = false;
 		private Fruits.Fruit nextFruit;
+
+		private bool isDrag = false;
 
 		private void Awake()
 		{
@@ -25,15 +32,17 @@ namespace Game
 		{
 			isClickable = true;
 
-			var startY = mainCamera.orthographicSize - fruitMesh[^1].bounds.size.y;
+			var startY = mainCamera.orthographicSize - fruitMesh[^1].bounds.size.y*100.0f;
+			Logger.Debug(startY);
 			startPos = new Vector3(0f, startY, 0f);
-			nextFruit = fruitManager.GetNewFruitForDrag(startPos);
+			nextFruit = fruitManager.GetNewFruitForShow(startPos);
+			SetBoxBound2D(mainCamera);
 		}
 
 		private void Update()
 		{
 			//Game
-			DragFruits();
+			//DragFruits();
 		}
 
 		private void DragFruits()
@@ -46,15 +55,53 @@ namespace Game
 			}
 		}
 
+		private IEnumerator DropFruitEverySecond()
+		{
+			while (isDrag)
+			{
+				yield return new WaitForSeconds(1.0f);
+				var newPosX = Random.Range(-2f, 2f);
+				var newPos = new Vector3(newPosX, startPos.y, 0f);
+				nextFruit.gameObject.SetActive(false);
+				fruitManager.GetFruitForDrag(nextFruit.FruitPoint, newPos);
+				nextFruit = fruitManager.GetNewFruitForShow(startPos);
+			}
+		}
+
+		public void OnDragFruitButtonClick()
+		{
+			isDrag = true;
+			StartCoroutine(DropFruitEverySecond());
+		}
+
+		public void OnStopDropFruitButtonClick()
+		{
+			isDrag = false;
+		}
+
 		private IEnumerator Drag(Vector3 pos)
 		{
 			isClickable = false;
 			nextFruit.gameObject.SetActive(false);
-			fruitManager.GetFruitForCombine(nextFruit.FruitPoint, pos);
+			fruitManager.GetFruitForDrag(nextFruit.FruitPoint, pos);
 			yield return new WaitForSeconds(1.5f);
-			nextFruit = fruitManager.GetNewFruitForDrag(startPos);
+			nextFruit = fruitManager.GetNewFruitForShow(startPos);
 			isClickable = true;
 		}
-		public List<Material> FruitMaterials => fruitMaterials;
+
+		private void SetBoxBound2D(Camera mainCamera)
+		{
+			float screenHeight = mainCamera.orthographicSize * 2f;
+			float screenWidth = screenHeight * mainCamera.aspect;
+			SetBoundPosition2D(topCollider, new Vector2(screenWidth, 0.01f), new Vector3(0f, screenHeight/2f - 100.0f*fruitMesh[^1].bounds.size.y, 0f));
+			SetBoundPosition2D(leftCollider, new Vector2(0.01f, screenHeight), new Vector3(-screenWidth / 2f, 0f, 0f));
+			SetBoundPosition2D(rightCollider, new Vector2(0.01f, screenHeight), new Vector3(screenWidth / 2f, 0f, 0f));
+		}
+
+		private void SetBoundPosition2D(BoxCollider2D collider2D, Vector2 size, Vector3 localPos)
+		{
+			collider2D.size = size;
+			collider2D.transform.localPosition = localPos;
+		}
 	}
 }
