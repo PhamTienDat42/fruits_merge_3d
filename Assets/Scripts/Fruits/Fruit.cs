d864768b1a7a4a49eccfeb5234eaff81292d28ee
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-29 19:11:28 +0700

    removed unused method in Fruit scripts

--- Assets/Scripts/Fruits/Fruit.cs
+++ Assets/Scripts/Fruits/Fruit.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using Game;
-using Pools;
 using UnityEngine;
 
 namespace Fruits
@@ -9,74 +5,6 @@ namespace Fruits
 	public class Fruit : MonoBehaviour
 	{
 		[SerializeField] private int fruitPoint;
-		private GameController controller;
-		private GameView gameView;
-		private FruitManager fruitManager;
-
-		private Rigidbody rb;
-
-		private const string FruitManagerTag = "FruitManager";
-
-		private void Start()
-		{
-			rb = GetComponent<Rigidbody>();
-			var fruitManagerObj = GameObject.FindGameObjectWithTag(FruitManagerTag);
-			fruitManager = fruitManagerObj.GetComponent<FruitManager>();
-		}
-
-		//public void InstantiateFruits(FruitPools fruitPools, GameController gameController, GameView gameView)
-		//{
-		//	this.fruitPools = fruitPools;
-		//	this.controller = gameController;
-		//	this.gameView = gameView;
-		//}
-
-		private void OnCollisionEnter(Collision collision)
-		{
-			Fruit otherFruit = collision.gameObject.GetComponent<Fruit>();
-
-			if (otherFruit != null)
-			{
-				if (CanCombine(otherFruit))
-				{
-					CombineFruit(otherFruit);
-				}
-			}
-		}
-
-		private bool CanCombine(Fruit otherFruit)
-		{
-			return fruitPoint == otherFruit.fruitPoint;
-		}
-
-		private void CombineFruit(Fruit otherFruit)
-		{
-			Fruit higherFruit = (transform.position.y > otherFruit.transform.position.y) ? this : otherFruit;
-			var newVelocity =higherFruit.gameObject.GetComponent<Rigidbody>().velocity;
-			Vector3 newFruitPos = higherFruit.transform.position;
-			var newPoints = fruitPoint + 1;
-
-			if (!gameObject.activeSelf && !otherFruit.gameObject.activeSelf)
-			{
-				return;
-			}
-
-			this.rb.velocity = otherFruit.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-			this.gameObject.SetActive(false);
-			otherFruit.gameObject.SetActive(false);
-
-			if(fruitPoint == 9)
-			{
-				return;
-			}
-
-			Fruit newFruit = fruitManager.GetFruitForCombine(newPoints, newFruitPos);
-			newFruit.GetComponent<Rigidbody>().velocity = newVelocity;
-			//if (Math.Abs(newVelocity.y) < 0.5f && Math.Abs(newVelocity.x) < 0.1f)
-			//{
-			//	newFruit.GetComponent<Rigidbody>().AddForce(Vector2.up, ForceMode.Impulse);
-			//}
-		}
 
 		public int FruitPoint { get => fruitPoint; set => fruitPoint = value; }
 	}
