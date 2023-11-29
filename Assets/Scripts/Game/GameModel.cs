a67bf926a64d8e9cbff092068bacd5ef17ef9065
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-29 11:23:29 +0700

    add currentscore for count score user

--- Assets/Scripts/Game/GameModel.cs
+++ Assets/Scripts/Game/GameModel.cs
@@ -4,6 +4,12 @@ namespace Game
 {
 	public class GameModel : MonoBehaviour
 	{
+		private int currentScore;
+		private void Start()
+		{
+			currentScore = 0;
+		}
 
+		public int CurrentScore { get => currentScore; set { currentScore = value;} }
 	}
 }
