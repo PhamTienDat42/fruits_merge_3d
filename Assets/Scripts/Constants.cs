e3d7522319fbc0e3d78ff0c4431d7ef599bb23a2
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-29 16:29:26 +0700

    add constants popup tag

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,6 +3,7 @@ public class Constants
 	public const string EntryScene = "Scenes/Entry";
 	public const string GameScene = "Scenes/Game";
 	public const string HomeScene = "Scenes/Home";
+	public const string Popup = "Scenes/Popup";
 
 	//Tags
 	public const string ServicesTag = "Services";
