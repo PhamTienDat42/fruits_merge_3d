308a6d9fe54e2edec240f657872d0627cea0406a
PhamTienDat42 <117574501+PhamTienDat42@users.noreply.github.com>
2023-12-02 22:38:12 +0700

    add save playerprefs when choose theme in home for test

--- Assets/Scripts/Home/HomeView.cs
+++ Assets/Scripts/Home/HomeView.cs
@@ -7,7 +7,18 @@ namespace Home
 	{
 		public void OnPlayButtonClick()
 		{
+			Time.timeScale = 1.0f;
 			SceneManager.LoadScene(Constants.GameScene);
+		}
+
+		public void OnBilliardThemeButtonClick()
+		{
+			PlayerPrefs.SetInt(Constants.Theme, 0);
+		}
+
+		public void OnSportThemeButtonClick()
+		{
+			PlayerPrefs.SetInt(Constants.Theme, 1);
 		}
 	}
 }
