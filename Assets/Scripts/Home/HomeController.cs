f41f4ca47dbb8cc33c9a766995c9169767f8c1c6
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-12-04 15:37:21 +0700

    add button for continue and get gameservice for get param to check user continue or not

--- Assets/Scripts/Home/HomeController.cs
+++ Assets/Scripts/Home/HomeController.cs
@@ -1,9 +1,24 @@
+using Services;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Home
 {
 	public class HomeController : MonoBehaviour
 	{
+		public GameServices GameServices { get; set; }
 
+		private void Awake()
+		{
+			var gameServiceObj = GameObject.FindGameObjectWithTag(Constants.ServicesTag);
+			if (gameServiceObj != null)
+			{
+				GameServices = gameServiceObj.GetComponent<GameServices>();
+			}
+			else
+			{
+				SceneManager.LoadScene(Constants.EntryScene);
+			}
+		}
 	}
 }
--- Assets/Scripts/Home/HomeView.cs
+++ Assets/Scripts/Home/HomeView.cs
@@ -1,3 +1,4 @@
+using Services;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,9 +6,19 @@ namespace Home
 {
 	public class HomeView : MonoBehaviour
 	{
+		[SerializeField] private HomeController homeController;
+
+		private ParamServices paramServices;
+
+		private void Start()
+		{
+			paramServices = homeController.GameServices.GetService<ParamServices>();
+		}
+
 		public void OnPlayButtonClick()
 		{
 			Time.timeScale = 1.0f;
+			paramServices.IsContinue = false;
 			SceneManager.LoadScene(Constants.GameScene);
 		}
 
@@ -19,6 +30,13 @@ namespace Home
 		public void OnSportThemeButtonClick()
 		{
 			PlayerPrefs.SetInt(Constants.Theme, 1);
+		}
+
+		public void OnContinueButtonClick()
+		{
+			Time.timeScale = 1.0f;
+			paramServices.IsContinue = true;
+			SceneManager.LoadScene(Constants.GameScene);
 		}
 	}
 }
